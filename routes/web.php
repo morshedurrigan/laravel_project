bfe0ab82f1f5d8d4f25bb982d58bd91d0f582ca9
Morshedur Rigan Sagar <morshedurrigansagar@Morshedurs-MacBook-Air.local>
2025-01-05 18:51:25 +0600

    Add Greeting route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Hello World ";
 });
+Route::get('/greeting', function () {
+    return view('greeting');
+});
+Route::get('/users', function () {
+    return view('users');
+});
+
